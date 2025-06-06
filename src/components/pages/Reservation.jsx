4591bea2f91a29fbe6726fe2d56553780eb31f28
Athanase Kpan <athanasesura1@gmail.com>
2025-06-05 21:29:00 -0400

    ajoute des input dans la page reservation et creation des autres pages et quelques amélioration

--- src/components/pages/Reservation.jsx
+++ src/components/pages/Reservation.jsx
@@ -8,58 +8,135 @@ export default function Reservation() {
   const [click2, setClick2] = useState(false);
   const [click3, setClick3] = useState(false);
   const [click4, setClick4] = useState(false);
+  const [suivant, setSuivant] = useState(false);
+  function handleClick() {
+    setClick1(!click1);
+  }
+  function handleClick2() {
+    setClick2(!click2);
+  }
+  function handleClick3() {
+    setClick3(!click3);
+  }
+  function handleClick4() {
+    setClick4(!click4);
+  }
+  function handleSuivant(){
+    if(suivant){
+      alert("hello form pret à envoyé")
+      setSuivant(false)
+    } else{
+      setSuivant(true)
+    }
+  }
   return (
     <div className="Reservation-container">
       <div className="section-form">
         <h1>Reservation</h1>
         <form action="">
-          <div className="input-groupe">
-            <h2>Date</h2>
-            <div>
-              <i className="bi bi-calendar"></i>
-              <input type="number" name="" id="" />
-              <i
-                className={click1 ? "bi bi-chevron-up" : "bi bi-chevron-down"}
-              ></i>
+          <h1>{!suivant ? "Detail sur la reservation!" : "Info personnelle!"}</h1>
+          {!suivant ? 
+          <div className="section1">
+            <div className="form-groupe">
+              <h2>Date</h2>
+              <div className="input-groupe">
+                <div className="icon">
+                  <i className="bi bi-calendar"></i>
+                </div>
+                <input type="number" name="" id="" />
+                <div className="icon">
+                  <i
+                    onClick={handleClick}
+                    className={
+                      click1 ? "bi bi-chevron-up" : "bi bi-chevron-down"
+                    }
+                  ></i>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="input-groupe">
-            <h2>Nombre de personne</h2>
-            <div>
-              <i className="bi bi-people"></i>
-              <input type="date" name="" id="" />
-              <i
-                className={click2 ? "bi bi-chevron-up" : "bi bi-chevron-down"}
-              ></i>
+            <div className="form-groupe">
+              <h2>Nombre de personne</h2>
+              <div className="input-groupe">
+                <div className="icon">
+                  <i className="bi bi-people"></i>
+                </div>
+                <input type="date" name="" id="" />
+                <div className="icon">
+                  <i
+                    onClick={handleClick2}
+                    className={
+                      click2 ? "bi bi-chevron-up" : "bi bi-chevron-down"
+                    }
+                  ></i>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="input-groupe">
-            <h2>Occasion</h2>
-            <div>
-              <i className="bi bi-cup-straw"></i>
-              <input type="text" name="" id="" />
-              <i
-                className={click3 ? "bi bi-chevron-up" : "bi bi-chevron-down"}
-              ></i>
+            <div className="form-groupe">
+              <h2>Occasion</h2>
+              <div className="input-groupe">
+                <div className="icon">
+                  <i className="bi bi-cup-straw"></i>
+                </div>
+                <input type="text" defaultValue="Anniversaire" name="" id="" />
+                <div className="icon">
+                  <i
+                    onClick={handleClick3}
+                    className={
+                      click3 ? "bi bi-chevron-up" : "bi bi-chevron-down"
+                    }
+                  ></i>
+                </div>
+              </div>
             </div>
-          </div>
 
-          <div className="input-groupe">
-            <h2>Heure</h2>
-            <div>
-              <i className="bi bi-clock"></i>
-              <input type="text" />
-              <i
-                className={click4 ? "bi bi-chevron-up" : "bi bi-chevron-down"}
-              ></i>
+            <div className="form-groupe">
+              <h2>Heure</h2>
+              <div className="input-groupe">
+                <div className="icon">
+                  <i className="bi bi-clock"></i>
+                </div>
+                <input type="text" />
+                <div className="icon">
+                  <i
+                    onClick={handleClick4}
+                    className={
+                      click4 ? "bi bi-chevron-up" : "bi bi-chevron-down"
+                    }
+                  ></i>
+                </div>
+              </div>
             </div>
+          </div>
+          :<div className="section2">
+            <div className="input-groupe1">
+              <h2>Nom</h2>
+              <input type="text" />
+            </div>
+            <div className="input-groupe1">
+              <h2>Prenom</h2>
+              <input type="text" />
+            </div>
+            <div className="input-groupe1">
+              <h2>Email</h2>
+              <input type="text" />
+            </div>
+            <div className="input-groupe1">
+              <h2>Numéro</h2>
+              <input type="text" />
+            </div>
+            <div className="input-groupe1">
+              <h2>Demande special</h2>
+              <textarea type="text" />
+            </div>
+          </div>
+          }
+           <button type="submit" onClick={handleSuivant}>{suivant ? "Submettre" : "Suivant"}</button>
+          <div className="plats">
+            <img src={Img} alt="image plat salad grec" />
+            <img src={Img2} alt="img  plat dessert lime" />
+            <img src={Img3} alt="plat saumon fumée" />
           </div>
         </form>
-      </div>
-      <div className="plats">
-        <img src={Img} alt="image plat salad grec" />
-        <img src={Img2} alt="img  plat dessert lime" />
-        <img src={Img3} alt="plat saumon fumée" />
       </div>
     </div>
   );
